e8a18ba9930b82916b8439358e261a8eb5459c2f
maniak <djmaniakfeatmcrybik@gmail.com>
2019-12-20 23:13:07 +0200

    Lesson12

--- src/Lesson12/VerarbeitenUser.php
+++ src/Lesson12/VerarbeitenUser.php
@@ -1,35 +1,12 @@
 <?php
 
-//echo __DIR__;die;
 
-$FileUserTxT = file_get_contents(__DIR__ . '/User1.txt');
+$fileUserTxt = file_get_contents(__DIR__ . '/User1.txt');
 
-$ExplodeUserTxT = explode('\n', $FileUserTxT);
-
-
-foreach ($ExplodeUserTxT as $Item) {
-    $Smash = str_replace(PHP_EOL, ' ', $Item);
-}
-
-
-$ExplodeSmash = explode(' ', $Smash);
-$FileUserTxTChunk = array_chunk($ExplodeSmash, 5);
+$explodeUserTxT = explode("\n", $fileUserTxt);
+$glue = implode(" ", $explodeUserTxT);
+$explodeUser = explode(" ", $glue);
+$FileUserTxTChunk = array_chunk($explodeUser, 5);
 
 //var_dump($FileUserTxTChunk);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
